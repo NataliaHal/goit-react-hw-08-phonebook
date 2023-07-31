1f89071e85344f98ca063b7a0c50925c7809d23c
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-07-31 11:07:51 +0200

    33333

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts.items; // масив з контактами
+export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter;
 export const selectError = state => state.contacts.error;
 export const selectIsLoading = state => state.contacts.isLoading;
